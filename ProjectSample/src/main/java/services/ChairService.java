d176c91d2139a7b0731bd1ee4eee65cfc33057f7
x14101629 <x14101629@OALAB-002.student.nci.col>
2018-04-12 20:06:15 +0100

    ChairService.java file updated

--- ProjectSample/src/main/java/services/ChairService.java
+++ ProjectSample/src/main/java/services/ChairService.java
@@ -19,10 +19,19 @@ public class ChairService extends Service {
     private final Timer timer;
     private int percentHot;
 
+    //try
+    private final Timer timer1;
+    //private int percentCool;
+    //end try
+
     public ChairService(String name) {
         super(name, "_chair._udp.local.");
         timer = new Timer();
         percentHot = 0;
+        //try
+        timer1 = new Timer();
+        percentHot = 0;
+        //end try
         ui = new ServiceUI(this, name);
     }
 
@@ -31,15 +40,20 @@ public class ChairService extends Service {
         if (a.equals("get_status")) {
             sendBack(getStatus());
         } else if (a.equals("Warm")) {
-            timer.schedule(new RemindTask(), 0, 1000);
+            timer.schedule(new RemindTaskWarm(), 0, 1000);
             sendBack("OK");
             ui.updateArea("Warming Chair");
+        } else if (a.equals("Cool")) {
+            timer1.schedule(new RemindTaskCool(), 0, 1000);
+            sendBack("OK");
+            ui.updateArea("Cooling Chair");
         } else {
             sendBack(BAD_COMMAND + " - " + a);
         }
+
     }
 
-    class RemindTask extends TimerTask {
+    class RemindTaskWarm extends TimerTask {
 
         @Override
         public void run() {
@@ -49,10 +63,29 @@ public class ChairService extends Service {
         }
     }
 
-    @Override
-    public String getStatus() {
+
+
+        class RemindTaskCool extends TimerTask {
+
+            @Override
+            public void run() {
+                if (percentHot > 20) {
+                    percentHot -= 1;
+                }
+            }
+        }
+        //end
+
+        @Override
+        public String getStatus
+        
+            () {
         return "Chair is " + percentHot + "'C Warm.";
-    }
+        }
+            
+    
+    
+
     
 
     public static void main(String[] args) {
