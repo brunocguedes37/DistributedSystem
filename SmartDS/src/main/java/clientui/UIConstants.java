921aef6dbc84e6fd7d4be34877d2f578bf8446f9
TurboNS <s.nuth@msn.com>
2018-04-15 12:21:48 +0100

    Delete UIConstants.java

--- SmartDS/src/main/java/clientui/UIConstants.java
+++
@@ -1,9 +0,0 @@
-package clientui;
-
-public class UIConstants {
-
-    public static final int UIWIDTH = 550;
-    public static final int UIHEIGHT = 600;
-    public static final int COMPONENTWIDTH = 520;
-    public static final int CONTROLY = 425;
-}
